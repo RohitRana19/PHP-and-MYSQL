a190713cf99693e33399a1305b0e2a5dc5208f77
R-code-ship <rohitrana0019@gmail.com>
2020-10-03 13:40:49 +0530

    Create q3_1_arrays.php

---
+++ Day1/q3_1_arrays.php
@@ -0,0 +1,14 @@
+<?php
+echo "DAYS IN A WEEK:  <br>";
+$a=array(1=>'Sunday',2=>'Monday',3=>'Tuesday',4=>'Wednesday',5=>'Thursday',6=>'Friday',7=>'Saturday');
+for($i=1;$i<=7;$i++)
+{
+  echo $i . "=>" . $a[$i] . "<br>";
+}
+
+
+
+
+
+
+ ?>
