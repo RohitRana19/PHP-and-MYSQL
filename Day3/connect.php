6bbe8e33f70fd95e784785bbcfaac5298f1b1da3
R-code-ship <rohitrana0019@gmail.com>
2020-10-03 13:41:25 +0530

    Create connect.php

---
+++ Day3/connect.php
@@ -0,0 +1,10 @@
+<?php
+$connect=mysqli_connect("localhost","root","","result") or die (mysqli_connect_error());
+//mysqli_connect_db($connect,"result") or die (mysqli_connect_error($connect));
+
+
+echo "CONNECTED SUCCESSFULLY!!;
+      <br><br>";
+
+
+ ?>
