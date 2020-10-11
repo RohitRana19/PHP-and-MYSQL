70e4dd2c73d8d6cc78bed1f96b20eeba2967dc8b
R-code-ship <rohitrana0019@gmail.com>
2020-10-11 14:27:56 +0530

    Delete q1_visitor_counter.php

--- Day 4/q1_visitor_counter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-        $file = file_get_contents("count.txt");
-	$visitor = $file;
-	$visitor_count = $visitor+1;
-	$new_file=fopen("count.txt","w");
-	fwrite ($new_file,$visitor_count);
-        echo "Number of visitors : $visitor";
-?>
